9aed097e66065a6dbb74ab03390af12da7e87b4d
HausBrunner <jason@brunner.haus>
2025-08-28 08:17:35 -0700

    remove

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/brunn3r/",
-  plugins: [react()],
-})
